4ea2a386b8919c1e66eef9cec24e61e8e63e7260
Muhammed Al-lami <105250911+m74e@users.noreply.github.com>
2025-06-21 10:35:26 +0300

    Update app.json and implement SearchBar component with tab navigation enhancements

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,9 +2,53 @@ import { View, Text, ImageBackground, Image } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
 import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
+
+const TabIcon = ({ focused, icon, title }: any) => {
+  if (focused) {
+    return (
+      <ImageBackground
+        source={images.highlight}
+        className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
+      >
+        <Image source={icon} tintColor="#151312" className="size-5"></Image>
+        <Text className="text-secondary text-base font-semibold ml-2">
+          {title}
+        </Text>
+      </ImageBackground>
+    );
+  }
+  return (
+    <View className="size-full justify-center items-center mt-4 rounded-full">
+      <Image source={icon} className="size-5" tintColor="#A8B5DB" />
+    </View>
+  );
+};
+
 const _layout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0f0D23",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          height: 52,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "#0f0D23",
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
@@ -12,25 +56,39 @@ const _layout = () => {
           headerShown: false,
           tabBarIcon: ({ focused }) => (
             <>
-              <ImageBackground source={images.highlight} />
-              <Image
-                source={images.home}
-                tintColor="#151312"
-                className="size-5"
-              ></Image>
-              <Text>Home</Text>
+              <TabIcon focused={focused} icon={icons.home} title="Home" />
             </>
           ),
         }}
       />
       <Tabs.Screen
         name="search"
-        options={{ title: "Search", headerShown: false }}
+        options={{
+          title: "Search",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.search} title="Search" />
+          ),
+        }}
       />
-      <Tabs.Screen name="saved" options={{ headerShown: false }} />
+      <Tabs.Screen
+        name="saved"
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.save} title="saved" />
+          ),
+        }}
+      />
       <Tabs.Screen
         name="profile"
-        options={{ title: "Profile", headerShown: false }}
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.person} title="Search" />
+          ),
+        }}
       />
     </Tabs>
   );
---
+++ components/searchBar.tsx
@@ -0,0 +1,30 @@
+import { View, Text, Image, TextInput } from "react-native";
+import React from "react";
+import { icons } from "@/constants/icons";
+
+interface Props {
+    onPress:()=>void;
+    placeholder: string;
+}
+const SearchBar = ({placeholder,onPress}:Props) => {
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#ab8bff"
+      />
+      <TextInput
+        onPress={() => {onPress()}}
+        placeholder={placeholder}
+        value=""
+        onChangeText={() => {}}
+        placeholderTextColor="#ab8bff"
+        className="flex-1 text-white ml-2"
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
