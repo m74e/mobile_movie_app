da810cc61f8e05e429567fb4fb749831cd18f50f
Muhammed Al-lami <105250911+m74e@users.noreply.github.com>
2025-06-21 15:27:51 +0300

    Refactor Index component to integrate useFetch for movie data and enhance loading/error handling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,18 @@
 import SearchBar from "@/components/searchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { fetchPMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
-import { Image, ScrollView, Text, View } from "react-native";
+import { ActivityIndicator, Image, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
-
   const router = useRouter();
-
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() => fetchPMovies({ query: "" }), false);
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0" />
@@ -18,13 +23,22 @@ export default function Index() {
         contentContainerStyle={{ paddingBottom: 10, minHeight: "100%" }}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-          <View className="flex-1 mt-5">
-        <SearchBar onPress={()=>router.push("/search")}
-          placeholder="Search for a movie or series"
+        {moviesLoading?(
+          <ActivityIndicator size="large"
+          color="#0000ff"
+          className="mt-10 self-center"
+          
           />
-      </View>
+        ): moviesError ? (
+          <></>
+        )}
+        <View className="flex-1 mt-5">
+          <SearchBar
+            onPress={() => router.push("/search")}
+            placeholder="Search for a movie or series"
+          />
+        </View>
       </ScrollView>
-    
     </View>
   );
 }
